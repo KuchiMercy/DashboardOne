ee4f57453410e44c322d5df5e9507accbe20f082
Mercy <durumercy206@gmail.com>
2024-10-24 15:00:54 +0100

    initial commit

---
+++ script/modalInspectionNotification.js
@@ -0,0 +1,26 @@
+const showModal = document.querySelector(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal2");
+const overlay = document.querySelector(".overlay2");
+
+function closeModals() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+showModal.addEventListener("click", openModal);
+
+closeModal.addEventListener("click", closeModals);
+
+overlay.addEventListener("click", closeModals);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModals();
+  }
+});
